4c88d6890d011d74a872d2dbc528a97d50313d3a
lanhai <boyu155115@163.com>
2019-06-03 15:38:48 +0800

    第五次提交 优化目前的项目代码 实现nginx负载均衡和高可用

--- jt-manage/src/main/java/com/jt/controller/MsgController.java
+++ jt-manage/src/main/java/com/jt/controller/MsgController.java
@@ -1,14 +1,15 @@
 package com.jt.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MsgController {
 
+    //查看服务器端口
     @RequestMapping("getMsg")
     public String getMsg(){
-        String str = "this is 8092";
+        String str = "this is 8093";
         System.out.println(str);
         return str;
     }
